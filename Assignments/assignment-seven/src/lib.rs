584cbcd85c8f0f559d0759602d7745e87bd4f54d
Aman Verma <60973491+aman09v@users.noreply.github.com>
2021-03-11 15:25:50 +0530

    adds meaningful comments

--- Assignments/assignment-seven/src/lib.rs
+++ Assignments/assignment-seven/src/lib.rs
@@ -5,6 +5,17 @@ pub mod ques1 {
     pub mod pattern_searching;
     pub mod substring_generation;
 }
+
+/// remove_unused_warnings function uses functions to remove warning
+///
+/// #Arguments
+///
+/// No arguments
+///
+/// #Return
+///
+/// No return type
+
 pub fn remove_unused_warning() {
     ques1::substring_generation::substring_generator("pa".to_string());
     ques1::pattern_searching::pattern_search("Pankaj Chaudhary".to_string(), "Cha".to_string());
